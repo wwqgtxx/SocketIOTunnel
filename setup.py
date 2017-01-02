1555cf515fa7b9796151426ce93a9c38f2c4a3ed
wwqgtxx <wwqgtxx@gmail.com>
2017-01-02 17:32:33 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,48 @@
 import sys
+VERSION = __import__('SocketIOTunnel').__version__
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 try:
     from cx_Freeze import setup, Executable
+
+    # Dependencies are automatically detected, but it might need fine tuning.
+    build_exe_options = {"include_files": ["libcrypto.dll", "libsodium.dll"],
+                         "packages": ["os", "sys", "gevent", "socketio", 'engineio'],
+                         "excludes": ["tkinter", "redis", "eventlet"],
+                         "zip_include_packages": [], "zip_exclude_packages": [], "include_msvcr": True}
+
+    # GUI applications require a different base on Windows (the default is for a
+    # console application).
+    base = None
+    # if sys.platform == "win32":
+    #     base = "Win32GUI"
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("client.py", base=base), Executable("server.py", base=base)]
+    setup(
+        name='SocketIOTunnel',
+        version=VERSION,
+        packages=['SocketIOTunnel', 'SocketIOTunnel.crypto'],
+        url='https://github.com/wwqgtxx/SocketIOTunnel',
+        license="GNU General Public License v3 (GPLv3)",
+        author='wwqgtxx',
+        author_email='wwqgtxx@gmail.com',
+        description='',
+        install_requires=REQUIREMENTS,
+        options={"build_exe": build_exe_options},
+        executables=[Executable("client.py", base=base), Executable("server.py", base=base)]
+    )
 except:
     from distutils.core import setup
 
-VERSION = __import__('SocketIOTunnel').__version__
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+    setup(
+        name='SocketIOTunnel',
+        version=VERSION,
+        packages=['SocketIOTunnel', 'SocketIOTunnel.crypto'],
+        url='https://github.com/wwqgtxx/SocketIOTunnel',
+        license="GNU General Public License v3 (GPLv3)",
+        author='wwqgtxx',
+        author_email='wwqgtxx@gmail.com',
+        description='',
+        install_requires=REQUIREMENTS
+    )
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"include_files": ["libcrypto.dll", "libsodium.dll"],
-                     "packages": ["os", "sys", "gevent", "socketio", 'engineio'],
-                     "excludes": ["tkinter", "redis", "eventlet"],
-                     "zip_include_packages": [], "zip_exclude_packages": [],"include_msvcr":True}
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-# if sys.platform == "win32":
-#     base = "Win32GUI"
-
-setup(
-    name='SocketIOTunnel',
-    version=VERSION,
-    packages=['SocketIOTunnel', 'SocketIOTunnel.crypto'],
-    url='https://github.com/wwqgtxx/SocketIOTunnel',
-    license="GNU General Public License v3 (GPLv3)",
-    author='wwqgtxx',
-    author_email='wwqgtxx@gmail.com',
-    description='',
-    install_requires=REQUIREMENTS,
-    options={"build_exe": build_exe_options},
-    executables=[Executable("client.py", base=base), Executable("server.py", base=base)]
-)
